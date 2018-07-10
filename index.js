aada288399600d28521ffee1554b9c26a41ea784
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2018-07-09 22:29:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here:
+function driversWithRevenueOver(arr, val){
+  return arr.filter(driver => driver.revenue > val);
+};
+
+function driverNamesWithRevenueOver(arr, val){
+  return driversWithRevenueOver(arr, val).map(driver => driver.name);
+};
+
+function exactMatch(arr, obj){
+  return arr.filter(driver => {
+    let matches = false;
+    for(key in driver){
+      if(driver[key] === obj[key])
+      matches = true;
+    }
+    return matches;
+  })
+};
+
+function exactMatchToList(arr, obj){
+  return exactMatch(arr, obj).map(driver => driver.name);
+};
